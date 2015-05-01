409d967ff308ab146e014f0a819c95f32c6d52cf
Jason Williams <jasonmw_nf@hotmail.com>
2015-05-01 12:24:16 -0230

    initial commit

---
+++ javascripts/game.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  var answer = Math.floor(Math.random() * 101),
+      guesses = 0;
+
+  $("form").on("submit", function(e) {
+    e.preventDefault();
+
+    var guess = +$("#guess").val(),
+        message;
+
+    guesses++;
+
+    if (guess === answer) {
+      message = "You guessed it! It took you " + guesses + " guesses.";
+    } 
+    else if (guess > answer) {
+      message = "My number is lower than " + guess;
+    }
+    else {
+      message = "My number is higher than " + guess;
+    }
+    $("p").text(message);
+  });
+
+  $("a").on("click", function(e) {
+    e.preventDefault();
+
+    answer = Math.floor(Math.random() * 101);
+    guesses = 0;
+    $("p").text("Guess a number from 1 to 100");
+  });
+});
